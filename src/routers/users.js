8166b28e6bb62182fe83f625663827e4b8f0b0b4
Brett <39827151+brett-scott@users.noreply.github.com>
2020-12-20 18:40:21 +1000

    Logout and Logout All routes
    - Users can now logout of a session or logout of all active sessions

--- src/routers/users.js
+++ src/routers/users.js
@@ -29,6 +29,33 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        //  Removed the token currently in use
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+
+        res.send()
+    } catch(e) {
+        res.status(500).send()
+    }
+});
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = [];
+
+        await req.user.save();
+    
+        res.send()
+    } catch(e) {
+        res.status(500).send()
+    }
+    
+})
+
 //  Get own profile
 router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
